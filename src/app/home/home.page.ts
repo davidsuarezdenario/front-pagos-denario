f2a56e26419e07ddf808e2877fc398d0384a9d4b
Sebastiang29 <sebastian.gomez10@gmail.com>
2024-03-24 03:17:52 -0500

    update: estilos currency

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -54,7 +54,8 @@ export class HomePage {
   validateAmount() {
     this.totalPayment = 0;
     this.credito ? this.totalPayment += this.credito : false;
-    this.denarios ? this.totalPayment += (this.denarios * 1000) : false;
+    /* this.denarios ? this.totalPayment += (this.denarios * 1000) : false; */
+    this.denarios ? (this.denarios > 500 ? this.totalPayment += (500 * 1000) : this.totalPayment += (this.denarios * 1000)) : false;
     this.totalPayment == this.dataBooking.Amount ? this.buttonPay = true : this.buttonPay = false;
   }
   payBooking() {
