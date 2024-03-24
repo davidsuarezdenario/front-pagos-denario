1b76f0cfe75db95e4239ca2fb36243f2bbcd1f6c
Sebastiang29 <sebastian.gomez10@gmail.com>
2024-03-23 21:27:49 -0500

    update: commit inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+  { path: 'home', loadComponent: () => import('./home/home.page').then((m) => m.HomePage), },
+  { path: 'home/:id', loadComponent: () => import('./home/home.page').then((m) => m.HomePage), },
+  { path: '', redirectTo: 'home', pathMatch: 'full', }
 ];
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,63 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonButton } from '@ionic/angular/standalone';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonButton]
 })
 export class HomePage {
-  constructor() {}
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+  headersWanderlust = { headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'denario2021' }) };
+  token = false;
+
+  ionViewWillEnter() {
+    this.token = this.route.snapshot.params['id'] ? true : false;
+    console.log('ionViewWillEnter: ', this.route.snapshot.params['id']);
+  }
+
+  async requestTransactionId(data: any) {
+    await this.processHttpRequest('post', '/transaction', { id: data }).then((res: any) => {
+      console.log(res);
+    }).catch((err: any) => {
+      console.log(err);
+    });
+  }
+  processHttpRequest(method: string, path: string, body: any) {
+    return new Promise((resolve, reject) => {
+      switch (method.toLowerCase()) {
+        case 'get':
+          this.http.get('localhost:3000' + path, this.headersWanderlust).subscribe((res: any) => {
+            console.log(res);
+            if (res.res == "ok") {
+              resolve(res.url);
+            } else {
+              resolve("error");
+            }
+          }, (err: any) => {
+            reject({ error: true, data: err });
+          });
+          break;
+        case 'post':
+          this.http.post('localhost:3000' + path, body, this.headersWanderlust).subscribe((res: any) => {
+            console.log(res);
+            if (res.res == "ok") {
+              resolve(res.url);
+            } else {
+              resolve("error");
+            }
+          }, (err: any) => {
+            reject({ error: true, data: err });
+          });
+          break;
+        default:
+          break;
+      }
+    });
+  }
 }
