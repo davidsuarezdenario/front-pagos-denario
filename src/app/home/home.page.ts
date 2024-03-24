8d21d01651f4fb592ad8ece16bf1afc016817cc7
Sebastiang29 <sebastian.gomez10@gmail.com>
2024-03-24 03:08:51 -0500

    update: currency pipe

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,18 +4,19 @@ import { CurrencyMaskModule } from "ng2-currency-mask";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonText, IonLabel, IonInput, IonButton } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [CommonModule, DatePipe, CurrencyPipe, CurrencyMaskModule, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonText, IonLabel, IonInput, IonButton]
+  imports: [CommonModule, DatePipe, CurrencyPipe, CurrencyMaskModule, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonText, IonLabel, IonInput, IonButton, FormsModule]
 })
 export class HomePage {
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
   headersWanderlust = { headers: new HttpHeaders({ 'Content-Type': 'application/json', 'x-api-key': 'viajesAuth' }) }; urlWanderlust = 'http://localhost:3000';
-  token = false; dataBooking: any = {};
+  token = false; buttonPay = false; dataBooking: any = {}; credito = undefined; denarios = undefined; totalPayment = 0;
 
   ionViewWillEnter() {
     this.route.snapshot.params['id'] ? this.requestTransactionId(this.route.snapshot.params['id']) : false;
@@ -23,7 +24,7 @@ export class HomePage {
 
   async requestTransactionId(data: any) {
     await this.processHttpRequest('post', '/travel/get_booking', { Id: data }).then((res: any) => {
-      if(res.error == false){ this.token = true, this.dataBooking = res.data; }
+      if (res.error == false) { this.token = true, this.dataBooking = res.data; }
     });
   }
   processHttpRequest(method: string, path: string, body: any) {
@@ -49,5 +50,14 @@ export class HomePage {
           break;
       }
     });
+  }
+  validateAmount() {
+    this.totalPayment = 0;
+    this.credito ? this.totalPayment += this.credito : false;
+    this.denarios ? this.totalPayment += (this.denarios * 1000) : false;
+    this.totalPayment == this.dataBooking.Amount ? this.buttonPay = true : this.buttonPay = false;
+  }
+  payBooking() {
+    console.log('Paying booking...');
   }
 }
